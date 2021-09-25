1dce857f1722261412dc5da44faf58c3928ffefd
aryansingh786420 <73500307+aryansingh786420@users.noreply.github.com>
2021-09-25 21:49:26 +0530

    Add files via upload

---
+++ Online Study Point/js/resp.js
@@ -0,0 +1,17 @@
+burger= document.querySelector('.burger')
+navbar= document.querySelector('.navbar')
+navList= document.querySelector('.nav-list')
+
+
+
+
+burger.addEventListener('click',()=>{
+    // navbar.classList.toggle('v-class');
+    // navbar.classList.toggle('v-class');
+    navList.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+
+
+
+})
+
